d55775624b9427af2de0b9d739ae6f031d6ecef6
Icaro Freire <ivfreire@outlook.com>
2026-08-16 07:57:07 -0300

    updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-$("div.image-gallery > div > img").on("click", () => {
-    
+
+$('div.image-gallery img').each(function (index) {
+    if ($(this).attr('onclick') != null) {                    
+        if ($(this).attr('onclick').indexOf("runThis()") == -1) {                        
+            $(this).click(function () {
+                $(this).attr('onclick');
+                var src = $(this).attr("src");
+                ShowLargeImage(src);
+            });
+        }
+    }
+    else {                    
+        $(this).click(function () {                        
+            var src = $(this).attr("src");
+            ShowLargeImage(src);
+        });
+    }
 });
+
+$('body').on('click', '.modal-overlay', function () {
+    $('.modal-overlay, .modal-img').remove();
+});
+
+function ShowLargeImage(imagePath) {
+    $('body').append('<div class="modal-overlay"><div class="modal-img"><img src="' + imagePath.replace("small","large") + '" /></div></div>');
+}
