98b32b544fbb11b5020af5f8df1ffc1c2389f360
Icaro Freire <ivfreire@outlook.com>
2026-08-26 08:59:22 -0300

    extending messaging system to sketchbook page

--- js/sketchbook.js
+++ js/sketchbook.js
@@ -1,8 +1,8 @@
 
+$("button#activate-message-writer").on("click", () => $("div#message-writer").toggle());
 
-
-$("#flipbook").turn({
-    width: $("div.sketchbook").width(),
-    height: $("div.sketchbook").width() / 2,
-    autoCenter: true
-});
+// $("#flipbook").turn({
+//     width: $("div.sketchbook").width(),
+//     height: $("div.sketchbook").width() / 2,
+//     autoCenter: true
+// });
