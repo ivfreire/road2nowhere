eac8294dab357fb0a1366a691139db1df4efb2ff
Icaro Freire <ivfreire@outlook.com>
2026-08-28 21:15:52 -0300

    sketchbook animation

--- js/sketchbook.js
+++ js/sketchbook.js
@@ -1,2 +1,15 @@
 
 $("button#activate-message-writer").on("click", () => $("div#message-writer").toggle());
+
+
+
+window.onmousemove = function(ev) {
+    let dx = -(ev.clientX - window.innerWidth / 2) / 30;
+    let dy = (ev.clientY - window.innerHeight / 2) / 15;
+    let deg = Math.sqrt(dx*dx + dy*dy);
+
+    $("div.header").css(
+        "transform",
+        `rotate3d(${dy}, ${dx}, 1, ${deg}deg)`
+    );
+}
